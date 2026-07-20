85f08b3f231f4cad73942c7d2abace67c8094fa0
Astha <aasthageda@gmail.com>
2026-07-21 00:38:09 +0530

    changing loader

--- models/loader.py
+++ models/loader.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 from pathlib import Path
 
 from peft import PeftModel
@@ -6,11 +7,24 @@ from transformers import AutoModelForSeq2SeqLM, AutoTokenizer
 
 MODEL_NAME = "facebook/bart-large-cnn"
 BASE_DIR = Path(__file__).resolve().parents[1]
-
 DEFAULT_ADAPTER = BASE_DIR / "training" / "lora_adapter_v2"
-adapter_path = Path(os.environ.get("SUMMARIZER_ADAPTER_PATH", DEFAULT_ADAPTER))
 
-tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
-base_model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
-model = PeftModel.from_pretrained(base_model, str(adapter_path)) if adapter_path.is_dir() else base_model
-model.eval()
+adapter_path = Path(
+    os.environ.get("SUMMARIZER_ADAPTER_PATH", DEFAULT_ADAPTER)
+)
+
+try:
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    base_model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
+    if adapter_path.is_dir():
+        model = PeftModel.from_pretrained(
+            base_model,
+            str(adapter_path)
+        )
+    else:
+        model = base_model
+
+    model.eval()
+except Exception:
+    traceback.print_exc()
+    raise
