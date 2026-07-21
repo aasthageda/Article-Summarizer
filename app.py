dbfa1abeac4c93e14289d0884351c1659add5482
Astha <aasthageda@gmail.com>
2026-07-21 20:56:42 +0530

    some updates

--- app.py
+++ app.py
@@ -1,14 +1,7 @@
+from frontend.ui import demo
 import os
 
-print("APP.PY STARTED")
-
-print("Importing frontend.ui...")
-from frontend.ui import demo
-print("frontend.ui imported successfully.")
-
-if __name__ == "__main__":
-    print("Launching Gradio...")
-    demo.launch(
-        server_name="0.0.0.0",
-        server_port=int(os.environ.get("PORT", 7860))
-    )
+demo.launch(
+    server_name="0.0.0.0",
+    server_port=int(os.environ.get("PORT", 7860))
+)
--- models/loader.py
+++ models/loader.py
@@ -1,15 +1,8 @@
 import os
 import traceback
 from pathlib import Path
-
-print("1. loader.py imported")
-
 from peft import PeftModel
-print("2. peft imported")
-
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
-print("3. transformers imported")
-
 MODEL_NAME = "facebook/bart-large-cnn"
 
 BASE_DIR = Path(__file__).resolve().parents[1]
@@ -20,22 +13,18 @@ adapter_path = Path(
 )
 
 try:
-    print("4. Loading tokenizer...")
     tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
 
-    print("5. Loading base model...")
-    base_model = AutoModelForSeq2SeqLM.from_pretrained(MODEL_NAME)
-
-    print("6. Adapter exists:", adapter_path.exists())
+    base_model = AutoModelForSeq2SeqLM.from_pretrained(
+        MODEL_NAME,
+        low_cpu_mem_usage=True
+    )
 
     if adapter_path.is_dir():
-        print("7. Loading LoRA adapter...")
         model = PeftModel.from_pretrained(base_model, str(adapter_path))
     else:
-        print("7. Using base model")
         model = base_model
 
-    print("8. Model ready")
     model.eval()
 
 except Exception:
