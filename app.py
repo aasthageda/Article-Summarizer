7aeac2787b0785a2758c67101df258f41e6bf99f
Astha <aasthageda@gmail.com>
2026-07-21 19:43:26 +0530

    update app.py

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
-print("APP.PY STARTED")
-
-from frontend.ui import demo
 import os
 
-print("Launching Gradio...")
+print("APP.PY STARTED")
+
+print("Importing frontend.ui...")
+from frontend.ui import demo
+print("frontend.ui imported successfully.")
 
 if __name__ == "__main__":
+    print("Launching Gradio...")
     demo.launch(
         server_name="0.0.0.0",
         server_port=int(os.environ.get("PORT", 7860))
